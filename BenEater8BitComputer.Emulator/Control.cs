b10bae3632ce8c15b8680ae5da26a02e6c27d75c
Fredi Machado <fredisoft@gmail.com>
2021-12-01 00:34:40 +1100

    Allow microcode to be reused

--- BenEater8BitComputer.Emulator/Control.cs
+++ BenEater8BitComputer.Emulator/Control.cs
@@ -11,51 +11,46 @@ public class Control : Component
     private readonly Ir instructionRegister;
     private readonly Stepper stepperRegister;
 
-    private readonly ControlLineFlags[] microcode;
+    internal static C[] Microcode { get; } = new C[]
+    {
+            // 0000 - NOP
+            C.MI | C.CO, C.RO | C.II | C.CE, 0,           0,           0,                  0, 0, 0,
+            // 0001 - LDA
+            C.MI | C.CO, C.RO | C.II | C.CE, C.IO | C.MI, C.RO | C.AI, 0,                  0, 0, 0,
+            // 0010 - ADD
+            C.MI | C.CO, C.RO | C.II | C.CE, C.IO | C.MI, C.RO | C.BI, C.EO | C.AI,        0, 0, 0,
+            // 0011 - SUB
+            C.MI | C.CO, C.RO | C.II | C.CE, C.IO | C.MI, C.RO | C.BI, C.EO | C.AI | C.SU, 0, 0, 0,
+            // 0100 - STA
+            C.MI | C.CO, C.RO | C.II | C.CE, C.IO | C.MI, C.AO | C.RI, 0,                  0, 0, 0,
+            // 0101 - LDI
+            C.MI | C.CO, C.RO | C.II | C.CE, C.IO | C.AI, 0,           0,                  0, 0, 0,
+            // 0110 - JMP
+            C.MI | C.CO, C.RO | C.II | C.CE, C.IO | C.J,  0,           0,                  0, 0, 0,
+            // 0111
+            C.MI | C.CO, C.RO | C.II | C.CE, 0,           0,           0,                  0, 0, 0,
+            // 1000
+            C.MI | C.CO, C.RO | C.II | C.CE, 0,           0,           0,                  0, 0, 0,
+            // 1001
+            C.MI | C.CO, C.RO | C.II | C.CE, 0,           0,           0,                  0, 0, 0,
+            // 1010
+            C.MI | C.CO, C.RO | C.II | C.CE, 0,           0,           0,                  0, 0, 0,
+            // 1011
+            C.MI | C.CO, C.RO | C.II | C.CE, 0,           0,           0,                  0, 0, 0,
+            // 1100
+            C.MI | C.CO, C.RO | C.II | C.CE, 0,           0,           0,                  0, 0, 0,
+            // 1101
+            C.MI | C.CO, C.RO | C.II | C.CE, 0,           0,           0,                  0, 0, 0,
+            // 1110 - OUT
+            C.MI | C.CO, C.RO | C.II | C.CE, C.AO | C.OI, 0,           0,                  0, 0, 0,
+            // 1111 - HLT
+            C.MI | C.CO, C.RO | C.II | C.CE, C.HLT,       0,           0,                  0, 0, 0,
+    };
 
     public Control(Bus bus, Ir instructionRegister, Stepper stepperRegister) : base(bus)
     {
         this.instructionRegister = instructionRegister;
         this.stepperRegister = stepperRegister;
-
-        var fetchT0 = C.MI | C.CO;
-        var fetchT1 = C.RO | C.II | C.CE;
-
-        microcode = new C[]
-        {
-                // 0000 - NOP
-                fetchT0, fetchT1, 0,           0,           0,                  0, 0, 0,
-                // 0001 - LDA
-                fetchT0, fetchT1, C.IO | C.MI, C.RO | C.AI, 0,                  0, 0, 0,
-                // 0010 - ADD
-                fetchT0, fetchT1, C.IO | C.MI, C.RO | C.BI, C.EO | C.AI,        0, 0, 0,
-                // 0011 - SUB
-                fetchT0, fetchT1, C.IO | C.MI, C.RO | C.BI, C.EO | C.AI | C.SU, 0, 0, 0,
-                // 0100 - STA
-                fetchT0, fetchT1, C.IO | C.MI, C.AO | C.RI, 0,                  0, 0, 0,
-                // 0101 - LDI
-                fetchT0, fetchT1, C.IO | C.AI, 0,           0,                  0, 0, 0,
-                // 0110 - JMP
-                fetchT0, fetchT1, C.IO | C.J,  0,           0,                  0, 0, 0,
-                // 0111
-                fetchT0, fetchT1, 0,           0,           0,                  0, 0, 0,
-                // 1000
-                fetchT0, fetchT1, 0,           0,           0,                  0, 0, 0,
-                // 1001
-                fetchT0, fetchT1, 0,           0,           0,                  0, 0, 0,
-                // 1010
-                fetchT0, fetchT1, 0,           0,           0,                  0, 0, 0,
-                // 1011
-                fetchT0, fetchT1, 0,           0,           0,                  0, 0, 0,
-                // 1100
-                fetchT0, fetchT1, 0,           0,           0,                  0, 0, 0,
-                // 1101
-                fetchT0, fetchT1, 0,           0,           0,                  0, 0, 0,
-                // 1110 - OUT
-                fetchT0, fetchT1, C.AO | C.OI, 0,           0,                  0, 0, 0,
-                // 1111 - HLT
-                fetchT0, fetchT1, C.HLT,       0,           0,                  0, 0, 0,
-        };
     }
 
     public override void FallingEdge()
@@ -65,7 +60,7 @@ public class Control : Component
         // Shift right instruction because step only uses 3 bits
         var index = (byte)(instruction >> 1 | (stepperRegister.Value & 0b111));
 
-        var controlLineFlags = microcode[index];
+        var controlLineFlags = Microcode[index];
         bus.SetControleLineFlags(controlLineFlags);
     }
 
